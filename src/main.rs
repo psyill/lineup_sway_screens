1e42fbcab055e454fe20cb7c864e6c093b06e01f
Hans Ellegård <psyill.net@gmail.com>
2025-06-26 12:32:30 +0200

    Avoid unnecessary copying by binding by reference

--- src/main.rs
+++ src/main.rs
@@ -28,14 +28,14 @@ struct Output {
     width: i32,
 }
 
-impl TryFrom<swayipc::Output> for Output {
+impl TryFrom<&swayipc::Output> for Output {
     type Error = Error;
 
-    fn try_from(value: swayipc::Output) -> Result<Self, Self::Error> {
+    fn try_from(value: &swayipc::Output) -> Result<Self, Self::Error> {
         value
             .current_mode
             .ok_or(Error::Plain("Output lacks current mode"))
-            .map(|mode| Output {
+            .map(|ref mode| Output {
                 name: value.name.clone(),
                 width: mode.width,
             })
@@ -45,8 +45,8 @@ impl TryFrom<swayipc::Output> for Output {
 fn main() -> Result<(), Error> {
     let mut connection = Connection::new().unwrap();
     let mut x: i32 = 0;
-    for output in get_sway_outputs(&mut connection)? {
-        set_sway_output_position(&mut connection, output.name, x, 0)?;
+    for ref output in get_sway_outputs(&mut connection)? {
+        set_sway_output_position(&mut connection, &output.name, x, 0)?;
         x += output.width;
     }
     Ok(())
@@ -56,14 +56,14 @@ fn get_sway_outputs(connection: &mut Connection) -> Result<Vec<Output>, Error> {
     Ok(connection
         .get_outputs()?
         .into_iter()
-        .map(|output| output.try_into().inspect_err(|e| eprintln!("{e}")))
+        .map(|ref output| output.try_into().inspect_err(|e| eprintln!("{e}")))
         .flatten()
         .collect())
 }
 
 fn set_sway_output_position(
     connection: &mut Connection,
-    screen_name: String,
+    screen_name: &impl std::fmt::Display,
     x: i32,
     y: i32,
 ) -> Result<(), Error> {
